1d13e0337c32e5e15431f58a444037b04d88e0c2
weeklyd3 <timothywei.spam@gmail.com>
2024-08-31 18:56:08 -0700

    actually screw it

--- script.js
+++ script.js
@@ -49,10 +49,6 @@ function drawHorizon(draw, width, height) {
 	}
 	draw.stroke('white');
 	draw.fill('white');
-	if (player.camera) {
-		draw.stroke('lime');
-		draw.fill('lime');
-	}
 	draw.strokeWeight(2);
 	draw.line(-width / 2, original_displacement, width / 2, original_displacement);
 	const start_hdg = Math.floor(player.heading / 10) * 10 - 140;
@@ -434,6 +430,3 @@ window.addEventListener('devicemotion', function(event) {
 window.addEventListener("deviceorientationabsolute", (ev) => {
 	player.heading = ev.alpha;
 });
-onerror = (err) => {
-	document.querySelector('#status').textContent = err.message;
-}
